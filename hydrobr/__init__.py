fbec0feb4e7893d19607239b0c0c1ed4aeb7a51a
Wallisson <cmwallisson@gmail.com>
2020-07-03 23:15:16 -0300

    Update version

--- hydrobr/__init__.py
+++ hydrobr/__init__.py
@@ -1,6 +1,6 @@
 """HydroBr is an open-source package to work with Brazilian hydrometeorological time series."""
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from hydrobr.get import Stations
 from hydrobr.graphics import Plot
