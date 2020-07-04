74c20e1b17db339269be7d1ff7523be21507a086
Wallisson <cmwallisson@gmail.com>
2020-07-03 23:05:44 -0300

    Update graphics and requirements

--- hydrobr/graphics.py
+++ hydrobr/graphics.py
@@ -12,15 +12,14 @@ class Plot:
         pass
 
     @staticmethod
-    def flow_duration_curve(data, y_log_scale=True, **kwargs):
+    def fdc(data, y_log_scale=True):
         """
         Make a flow duration curve plot.
         :param data: A Pandas daily DataFrame with DatetimeIndex where each column corresponds to a station.
         :param y_log_scale: boolean, optional, default: True, to set the plotting y-axis in the logarithmic scale.
-        Use others parameters from matplotlib.pyplot.figure as kwargs.
+        :return plotly figure
         """
-        plot_fdc = plt.figure(**kwargs)
-        plt.grid(True, which="both", ls="-")
+        fig = go.Figure()
         y_max = 0
         for name in data.columns:
             series = data[name].dropna()
@@ -30,28 +29,24 @@ class Plot:
             if y_max < y.max():
                 y_max = y.max()
             x = (np.arange(1, n + 1) / n) * 100
-            _ = plt.plot(x, y, linestyle='-')
-        plt.legend(list(data.columns), loc='best')
-        plt.margins(0.02)
+            fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name=name))
+
         if y_log_scale:
             ticks = 10 ** np.arange(1, ceil(log(y_max, 10)) + 1, 1)
             ticks[-1:] += 1
-            plt.yticks(list(ticks))
-            plt.yscale('log')
-            plt.tick_params(axis='y', which='minor')
-        plt.xticks(np.arange(0, 101, step=10))
-        return plot_fdc
+            fig.update_layout(yaxis=dict(
+                tickmode='array', tickvals=ticks, dtick=2), yaxis_type="log")
+        fig.update_layout(xaxis=dict(tickmode='array', tickvals=np.arange(0, 101, step=10)))
+        return fig
 
     @staticmethod
     def gantt(data, monthly=True):
         """
         Make the Gantt plot. This graphic shows the temporal data availability for each station.
         :param data:A Pandas daily DataFrame with DatetimeIndex where each column corresponds to a station.
-        :param graph_height: int, defines the height of the output graphic
-        :param graph_name: str, optional, default: True
-        Defines the name of the exported graph
-        :param monthly: boolean, optional, default: True
-        Defines if the availability count of the data will be monthly to obtain a more fluid graph.
+        :param monthly: boolean, optional, default: True, Defines if the availability count of the data will be monthly
+        to obtain a more fluid graph.
+        :return plotly figure
         """
         date_index = pd.date_range(data.index[0], data.index[-1], freq='D')
         data = data.reindex(date_index)
